788f42f8f905ee20bc2af043bb41ac1025f4556d
Charizza Thunjung <charizzathunjungsukmanaputra@mail.ugm.ac.id>
2023-10-05 14:58:05 +0700

    PPW2_A2_2_CharizzaThunjung

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
+use App\Http\Controllers\PostController;
 use Illuminate\Support\Facades\Route;
 
-//route resource
-Route::resource('/', \App\Http\Controllers\PostController::class);
+// //route resource
+Route::resource('posts', PostController::class);
